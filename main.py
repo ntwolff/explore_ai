f385e65563a2cd79ffec85cbff4d4020dbd5a374
Nick Wolff <nick.wolff@avant.com>
2023-11-09 23:22:48 +0000

    reorganizing

--- main.py
+++ main.py
@@ -1,142 +1,20 @@
-from flask import Flask, request
-import json
-from openai import OpenAI
-from weather import get_weather
-import time
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
+from flask import Flask
+from assistant_interactor import assist
 
 app = Flask(__name__)
 
-# Initialize OpenAI API
-# TODO: model_engine = "text-davinci-002"
-client = OpenAI(api_key = os.getenv("OPENAI_API_KEY"))
-
-# Initialize conversation history
-#conversation_history = []
-assistant_id = os.getenv('OPENAI_ASSISTANT_ID')
-
-functions = {
-    "get_weather": get_weather
-}
-
-# Load the personality context once from assistant_instructions.txt
-with open('assistant_instructions.txt', 'r') as file:
-    instructions = file.read().strip()
-
-# Function to interact with the chatbot
+# Home page
 @app.route("/")
-def interact_with_bot():
+def info():
+    return "<p>openai assistant - function calling demo</p>"
 
-    thread = client.beta.threads.create()
-    last_message_id = None
+# Start the chat
+@app.route("/start")
+def start_assisting():
+    # TODO
+    # assist()
 
-    while True:
-        question = input("user: ")
-        if question.lower() == "exit":
-            print("Exiting the chat.")
-            break
-        try:
-            # Add user message to the thread
-            message = client.beta.threads.messages.create(
-                thread_id=thread.id,
-                role="user",
-                content=question
-            )
-            
-            # Tag the message as seen
-            last_message_id = message.id
-
-            # Run assistant
-            run = client.beta.threads.runs.create(
-                thread_id=thread.id,
-                assistant_id=assistant_id,
-                instructions=instructions
-            )
-
-            # Display assistant response
-            run = client.beta.threads.runs.retrieve(
-                thread_id=thread.id,
-                run_id=run.id
-            )
-
-            # Wait until it is not queued
-            count = 0
-            while(run.status == "queued" or run.status == "in_progress") and count < 5:
-                time.sleep(2)
-                run = client.beta.threads.runs.retrieve(
-                    thread_id=thread.id,
-                    run_id=run.id
-                )
-                count = count + 1
-
-            if run.status == "requires_action":
-
-                # Get the tool outputs by executing the required functions
-                tool_outputs = execute_required_functions(run.required_action)
-
-                # Submit the tool outputs back to the Assistant
-                run = client.beta.threads.runs.submit_tool_outputs(
-                    thread_id=thread.id,
-                    run_id=run.id,
-                    tool_outputs=tool_outputs
-                )
-
-            # Wait until it is not queued
-            count = 0
-            while(run.status == "queued" or run.status == "in_progress" or run.status == "requires_action") and count < 5:
-                time.sleep(2)
-                run = client.beta.threads.runs.retrieve(
-                    thread_id=thread.id,
-                    run_id=run.id
-                )
-                count = count + 1
-
-            
-            # Retrieve messages from thread after run complete            
-            thread_messages = client.beta.threads.messages.list(
-                thread_id=thread.id
-                ,order="desc"
-                ,before=last_message_id
-            )
-            #app.logger.debug(thread_messages.data)
-
-            for message in thread_messages.data:
-                if message.role == "assistant":
-                    message_text = message.content[0].text.value
-                    print(f"\nassistant: {message_text}\n")
-                    last_message_id = message.id
-            
-        except Exception as e:
-            print(f"An error occurred: {e}")
-
-# Function to call the assistant required functions and return their outputs as JSON strings
-def execute_required_functions(required_actions):
-    tool_outputs = []
-    for tool_call in required_actions.submit_tool_outputs.tool_calls:
-        func_name = tool_call.function.name
-        args = json.loads(tool_call.function.arguments)
-
-        # Call the corresponding Python function
-        if func_name in functions:
-            function = functions[func_name]
-            # Assuming all functions take a single dictionary argument
-            result = function(**args)
-
-            # Serialize the function's output to JSON
-            result_str = json.dumps(result)
-
-            # Add the result to the list of tool outputs
-            tool_outputs.append({
-                "tool_call_id": tool_call.id,
-                "output": result_str,
-            })
-
-    return tool_outputs
-
-# home route that returns below text when root url is accessed
-# @app.route("/")
-# def info():
-#     return "<p>GPT Assistant Interactor App.</p>"
+# Stop the chat
+@app.route("/stop")
+def stop_assisting():
+    # TODO
--- weather.py
+++ weather.py
@@ -1,26 +1,21 @@
 import requests
-import os
-from dotenv import load_dotenv, find_dotenv
 
-BASEDIR = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(BASEDIR, '.env'))
-
-# Your API Key
-API_KEY = os.getenv('WEATHER_API_KEY')
-
-# Base URL for the Weather API
 WEATHER_BASE_URL = "https://api.weather.gov/points/"
 
-headers = {
-    "User-Agent": os.getenv('EMAIL_USER'),
-    "Accept": "application/vnd.noaa.dwml+xml;version=1"
-}
-
 def get_weather(latitude, longitude):
-    """Get the current weather for a location using the Weather API."""
+    """
+    Get the current weather for a location using the Weather API.
+
+    Args:
+    latitude (str): The latitude of the location.
+    longitude (str): The longitude of the location.
+
+    Returns:
+    dict: A dictionary containing the current weather information.
+    """
     url = WEATHER_BASE_URL + latitude + "," + longitude
+    headers = {"Accept": "application/geo+json"}
     response = requests.get(url, headers=headers)
-    # Check if the response was successful
     if response.status_code == 200:
         data = response.json()
         forecast_url = data["properties"]["forecast"]
@@ -28,6 +23,6 @@ def get_weather(latitude, longitude):
         if response.status_code == 200:
             return response.json()
         else:
-            return response.json()  # This will contain the error information.
+            return response.json()  
     else:
-        return response.json()  # This will contain the error information.
+        return response.json()
